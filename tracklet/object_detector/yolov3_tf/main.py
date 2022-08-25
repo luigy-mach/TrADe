e325957549143901c50f53e7932a396a0b9f40b9
luigy-mach <luigymachaca@gmail.com>
2022-08-25 00:40:01 -0500

    add YOLOv3 from https://github.com/wizyoung/YOLOv3_TensorFlow

---
+++ tracklet/object_detector/yolov3_tf/main.py
@@ -0,0 +1,8 @@
+from interface import object_detection
+
+
+
+id_ite, initBoundingBox = object_detection("/home/luigy/isabo/YOLOv3_TensorFlow/data/demo_data/messi.jpg")
+
+print (id_ite)
+print (initBoundingBox)
